52884d5802082c5d8414f1ec8f562e71dc1203a2
Fusa Fukushi <fusa.fukushi@intloop.com>
2024-07-04 18:43:16 +0900

    feat: importの検証

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import './sample'
+import { increment } from './sample';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload. {increment(90)}
         </p>
         <a
           className="App-link"
---
+++ frontend/src/sample.ts
@@ -0,0 +1,4 @@
+
+export function increment(num: number) {
+    return num + 1;
+}
